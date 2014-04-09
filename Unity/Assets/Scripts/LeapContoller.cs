9bcec5195ad9120282223e7a4a31ee9a8eff7def
Jonathan Bihet <jonathan.bihet@gmail.com>
2014-04-09 15:19:23 +0200

    Intégration Leap Motion

---
+++ Unity/Assets/Scripts/LeapContoller.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapContoller : MonoBehaviour
+{
+    Controller controller;
+    InteractionBox _iBox;
+    bool _bitchHasShot;
+    float coolDownValue;
+    float currCD;
+    bool cooldowner;
+    double yAvgPos;
+
+    void Start()
+    {
+        controller = new Controller();
+        _bitchHasShot = false;
+        coolDownValue = 0.5f;
+        currCD = 0.0f;
+        cooldowner = false;
+    }
+
+    void Update()
+    {
+        Frame frame = controller.Frame();
+
+        Vector avgPos = Vector.Zero;
+        foreach(Pointable pointable in frame.Pointables)
+        {
+            avgPos += pointable.TipPosition;
+        }
+        avgPos /= frame.Pointables.Count;
+        if(avgPos.y != float.NaN)
+        {
+            yAvgPos = avgPos.y;
+        }
+
+        if(yAvgPos < 40.0f && !_bitchHasShot && currCD == 0.0f)
+        {
+            Debug.Log("log that");
+            _bitchHasShot = true;
+            currCD = coolDownValue;
+        }
+        else if (yAvgPos > 40.0f && _bitchHasShot && currCD == 0.0f)
+        {
+            _bitchHasShot = false;
+        }
+
+        //if(!_bitchHasShot && frame.Pointables.Count == 0)
+        //{
+        //    _bitchHasShot = true;
+        //    currCD = coolDownValue;
+        //    Debug.Log("log that bitch");
+        //}
+        //else if(_bitchHasShot && frame.Pointables.Count != 0 && currCD == 0.0f)
+        //{
+        //    _bitchHasShot = false;
+        //}
+
+        if (currCD != 0.0f)
+        {
+            currCD -= Time.deltaTime;
+            if (currCD < 0.0f)
+                currCD = 0.0f;
+        }
+    }
+}
