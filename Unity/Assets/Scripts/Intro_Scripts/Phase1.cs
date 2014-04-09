d9444e87572847e7a4b68a5999a2accc9b07fd69
Jonathan Bihet <jonathan.bihet@gmail.com>
2014-04-09 21:06:48 +0200

    Menu & Intro, V1

---
+++ Unity/Assets/Scripts/Intro_Scripts/Phase1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Phase1 : MonoBehaviour
+{
+    float timer;
+    
+    [SerializeField]
+    GameObject phase2;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 12.0f)
+        {
+            this.gameObject.SetActive(false);
+            phase2.SetActive(true);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Intro_Scripts/spraypoper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spraypoper : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] sprayTexture;
+
+    int current;
+    float timer;
+    float stepper;
+    float decrementer;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0.0f;
+        current = 0;
+        stepper = 0.5f;
+        decrementer = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= stepper)
+        {  
+            if(current < sprayTexture.Length-1)
+            {
+                sprayTexture[current].SetActive(true);
+                current++;
+                stepper -= decrementer;
+                decrementer += decrementer/2;
+            }
+
+            timer = 0.0f;
+        }
+
+    }
+}
