3f375df520c1d720e1c3d947eed3daddd840ba92
frite333 <pierre.gaboriaud@gmail.com>
2014-04-09 22:08:58 +0200

    added: Server/Client starting connection

---
+++ Unity/Assets/Scripts/NetworkInterface.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transformSerializable
+{
+    public float tx;
+    public float ty;
+    public float rx;
+    public float ry;
+    public float rw;
+}
+
+public interface NetworkInterface {
+    void sendWishesToServer(bool state);
+    void receptWishesFromServer(byte[] wishes);
+}
