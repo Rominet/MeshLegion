1ccd99f9d0979a8706012039fd1e17fc4f27d1c5
Rominet <thierry.destribats@gmail.com>
2014-04-09 13:02:27 +0200

    [Add] Creation Maze Scene

---
+++ Unity/Assets/Scripts/NavManagerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavManagerScript : MonoBehaviour {
+
+    public NavMeshAgent navAgent;
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        navAgent.SetDestination(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
